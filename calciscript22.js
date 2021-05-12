5ef177502a87e135025ea0fc3e92ac499548dba5
RukhMaharshi-Manthu <82650031+RukhMaharshi-Manthu@users.noreply.github.com>
2021-05-12 20:45:01 +0530

    Add files via upload

---
+++ calciscript22.js
@@ -0,0 +1,26 @@
+function dis(val) 
+          { 
+              document.getElementById("result").value+=val 
+          } 
+            
+          function solve() 
+          { 
+              let x = document.getElementById("result").value 
+              let y = eval(x) 
+              document.getElementById("result").value = y 
+          } 
+            
+          function clr() 
+          { 
+              document.getElementById("result").value ="" 
+          }
+          function Aclr() 
+          { 
+              document.getElementById("result").value ="" 
+             
+          }
+          
+function back() {
+    var value = document.getElementById("result").value;
+    document.getElementById("result").value = value.substr(0, value.length - 1);
+}
